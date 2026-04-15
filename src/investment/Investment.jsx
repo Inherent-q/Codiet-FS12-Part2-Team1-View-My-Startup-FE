4d4f814041894b7c41cd6872f40dadd98772342c
apple <apple@apples-MacBook-Air.local>
2026-04-15 13:38:21 +0900

    feat: Investment.jsx 이미지 import 경로 수정 및 정렬 ui 구현

--- src/investment/Investment.jsx
+++ src/investment/Investment.jsx
@@ -42,26 +42,23 @@ const Investment = () => {
   //정렬
 
   const handleToggle = function () {
-    setToggle((toggle = !toggle));
+    setToggle(!toggle);
   };
 
-  const dorpdown = function () {
+  const dropdown = function () {
     if (toggle === true) {
       return (
-        <div>
-          <button>View My Startup 투자 금액 높은순</button>
-          <button>View My Startup 투자 금액 낮은순</button>
-          <button>실제 누적 투자 금액 높은순</button>
-          <button>실제 누적 투자 금액 낮은순</button>
+        <div className="sort-options">
+          <button className="vms-desc">View My Startup 투자 금액 높은순</button>
+          <button className="vms-asc">View My Startup 투자 금액 낮은순</button>
+          <button className="amount-desc">실제 누적 투자 금액 높은순</button>
+          <button className="amount-asc">실제 누적 투자 금액 낮은순</button>
         </div>
       );
     } else {
       return;
     }
   };
-
-  //1. 토글 버튼 클릭시 드롭다운 열리고 닫히기
-  //2. 드롭다운 열렸을 때 4가지 정렬 옵션 보여주기
 
   //3. 옵션 클릭하면 sort 변경되고, 드롭다운 닫힌다
   //4. 변경된 sort에 따라서 테이블 정렬이 바뀐다.
@@ -97,13 +94,14 @@ const Investment = () => {
     <div className="all-wrapper">
       <div className="title-sort-wrapper">
         <h1>투자 현황</h1>
-        <div className="dropdown">
-          <div className="dropdown-wrapper">
+        <div className="dropdown-wrapper">
+          <div className="dropdown-board">
             <span className="sort-text">View My Startup 투자 금액 높은순</span>
-            <button className="dropdown-toggle">
+            <button className="dropdown-toggle" onClick={handleToggle}>
               <img src={toggleImage} />
             </button>
           </div>
+          <div className="sort-options-wrapper">{dropdown()}</div>
         </div>
       </div>
       <div className="table-wrapper">
