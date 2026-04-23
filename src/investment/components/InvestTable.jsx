4b557de5309c96ea26f05bd9944eb4432f57e278
apple <apple@apples-MacBook-Air.local>
2026-04-23 09:39:47 +0900

    feat: 스켈레톤 리펙토링 및 데이터 로딩중 css 변경

--- src/investment/components/InvestTable.jsx
+++ src/investment/components/InvestTable.jsx
@@ -13,6 +13,8 @@ const InvestTable = ({
   setSort,
   setOrder,
 }) => {
+  const hasData = corps && corps.length > 0;
+
   return (
     <div className="all-wrapper">
       <div className="title-sort-wrapper">
@@ -25,29 +27,28 @@ const InvestTable = ({
         />
       </div>
       <div>
-        <div className={`table-wrapper${loading ? "loading" : ""}`}>
-          {loading && <div className="table-overlay" />}
-          {loading ? (
-            <InvestSkeletonTable />
-          ) : (
-            <table className="table">
-              <thead className="thead">
-                <tr>
-                  <th>순위</th>
-                  <th>기업명</th>
-                  <th>기업 소개</th>
-                  <th>카테고리</th>
-                  <th>
-                    View My Startup
-                    <br />
-                    투자 금액
-                  </th>
-                  <th>실제 누적 투자 금액</th>
-                </tr>
-              </thead>
+        <div className="table-wrapper">
+          <table className={`table ${loading ? "loading" : ""}`}>
+            <thead className="thead">
+              <tr>
+                <th>순위</th>
+                <th>기업명</th>
+                <th>기업 소개</th>
+                <th>카테고리</th>
+                <th>
+                  View My Startup
+                  <br />
+                  투자 금액
+                </th>
+                <th>실제 누적 투자 금액</th>
+              </tr>
+            </thead>
+            {loading && !hasData ? (
+              <InvestSkeletonTable />
+            ) : (
               <InvestCorpsList corps={corps} curPage={curPage} />
-            </table>
-          )}
+            )}
+          </table>
         </div>
       </div>
     </div>
